bae3d394b94874518ef7181ae0077bf17daaa58d
lilian <lilian.wangechi@student.moringaschool.com>
2022-10-21 05:38:56 +0300

    done

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,43 @@
 class Triangle
   # write code here
+  attr_accessor :a, :b, :c
+  
+
+  def initialize(a, b, c)
+    @a=a
+    @b=b
+    @c=c
+  end
+
+  def kind
+    validate_triangle
+
+    if a==b && b ==c
+      :equilateral
+    elsif a!=b && b!=c && c!=a
+      :scalene
+    else
+      :isosceles
+    end
+
+  end
+
+
+
+  def traingle_correct?
+    a + b > c && a + c > b && b + c > a
+  end
+  def postive_traingle?
+    [a,b,c].all?(&:positive?)
+  end
+
+def validate_triangle
+  raise TriangleError unless traingle_correct? && postive_traingle?
+end
+
+
+  class TriangleError < StandardError
+
+    # triangle error code
+  end
 end
